66bcff63044ca38576a14de2807b83d5f90d9991
DhairyaParikh31 <dhairyaparikh31@gmail.com>
2024-05-12 22:29:44 +0530

    commit

---
+++ js/Code-Canvas-Component.js
@@ -0,0 +1,28 @@
+let isSideBarOpen = true;
+document.getElementById("sideBarTargetBtn").addEventListener("click", sideBarToggleHandler);
+
+function sideBarToggleHandler() {
+    console.log("heworkin...");
+    const sidebar = document.getElementById("container");
+    const mainContent = document.getElementById("main-content");
+    if (isSideBarOpen) {
+        sidebar.classList.add("sidebar-hidden");
+        mainContent.classList.add("content-centered");
+    } else {
+        sidebar.classList.remove("sidebar-hidden");
+        mainContent.classList.remove("content-centered");
+    }
+    isSideBarOpen = !isSideBarOpen;
+}
+
+const links = document.querySelectorAll("#links .link");
+
+links.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        links.forEach(function(link) {
+            link.classList.remove("highlighted-link");
+        });
+        link.classList.add("highlighted-link");
+    });
+});
